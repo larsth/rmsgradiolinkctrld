aadd1abe50fd3763b4d80c096ce52edd2fed89c0
Lars Tørnes Hansen <devel@larsth.dk>
2015-09-18 05:45:09 +0200

    golint-ed readfrom_test.go in package github.com/larsth/rmsgradiolinkctrld/gps

--- gps/readfrom_test.go
+++ gps/readfrom_test.go
@@ -8,9 +8,9 @@ import (
 	"testing"
 )
 
-func AssertScanner_ReadFrom_N(got, want int64) error {
+func AssertScannerReadFromN(got, want int64) error {
 	var (
-		format string = "Scanner.RedFrom, n output: Got: n='%d', Want: n='%d'"
+		format = "Scanner.RedFrom, n output: Got: n='%d', Want: n='%d'"
 	)
 	if want != got {
 		return fmt.Errorf(format, got, want)
@@ -18,11 +18,11 @@ func AssertScanner_ReadFrom_N(got, want int64) error {
 	return nil
 }
 
-func AssertScanner_ReadFrom_Error(got, want error) error {
+func AssertScannerReadFromError(got, want error) error {
 	var (
 		gStr   string
 		wStr   string
-		format string = "Scanner.RedFrom, error output: Got err='%s', Want: err='%s'"
+		format = "Scanner.RedFrom, error output: Got err='%s', Want: err='%s'"
 	)
 	gStr = fmt.Sprintf("%s", got)
 	wStr = fmt.Sprintf("%s", want)
@@ -32,7 +32,7 @@ func AssertScanner_ReadFrom_Error(got, want error) error {
 	return nil
 }
 
-func AssertScanner_ReadFrom_StructFieldIoReader(got, want io.Reader) error {
+func AssertScannerReadFromStructFieldIoReader(got, want io.Reader) error {
 	format := "%s '%#v', Want struct field: err='%#v'"
 	s1 := "Scanner.RedFrom, comparing io.Readers: Got struct field '"
 
@@ -45,52 +45,52 @@ func AssertScanner_ReadFrom_StructFieldIoReader(got, want io.Reader) error {
 	return nil
 }
 
-func AssertScanner_ReadFrom_StructFieldC(got, want rune) error {
+func AssertScannerReadFromStructFieldC(got, want rune) error {
 	panic("FIXME Llaves færdig")
 	return nil
 }
 
-func TestScanner_ReadFromNilIoReader(t *testing.T) {
+func TestScannerReadFromNilIoReader(t *testing.T) {
 	var (
-		s      *Scanner = NewScanner(&gst_testing)
+		s      = NewScanner(&gst_testing)
 		gotN   int64
 		gotErr error
-		r      io.Reader = io.Reader(nil)
+		r      = io.Reader(nil)
 		err    error
 	)
 	gotN, gotErr = s.ReadFrom(r)
-	if err = AssertScanner_ReadFrom_N(gotN, 0); err != nil {
+	if err = AssertScannerReadFromN(gotN, 0); err != nil {
 		t.Error(err.Error())
 	}
-	if err = AssertScanner_ReadFrom_Error(gotErr, ErrIoNilReader); err != nil {
+	if err = AssertScannerReadFromError(gotErr, ErrIoNilReader); err != nil {
 		t.Error(err.Error())
 	}
 }
 
-func TestScanner_ReadFromStructFields(t *testing.T) {
+func TestScannerReadFromStructFields(t *testing.T) {
 	var (
-		s                *Scanner
-		buf_struct_field bytes.Buffer
-		buf_ioReader_arg bytes.Buffer
-		r                io.Reader
-		err              error
+		s              *Scanner
+		bufStructField bytes.Buffer
+		bufIoReaderArg bytes.Buffer
+		r              io.Reader
+		err            error
 	)
 
 	s = NewScanner(&gst_testing)
 
-	buf_struct_field.WriteString(" ")
-	s.ioReader = io.Reader(&buf_struct_field)
+	bufStructField.WriteString(" ")
+	s.ioReader = io.Reader(&bufStructField)
 
-	buf_ioReader_arg.WriteString("\"class\":\"TPV\"}")
-	r = io.Reader(&buf_ioReader_arg)
+	bufIoReaderArg.WriteString("\"class\":\"TPV\"}")
+	r = io.Reader(&bufIoReaderArg)
 
 	_, _ = s.ReadFrom(r)
 
-	if err = AssertScanner_ReadFrom_StructFieldIoReader(r, r); err != nil {
+	if err = AssertScannerReadFromStructFieldIoReader(r, r); err != nil {
 		t.Error(err.Error())
 	}
 
-	if err = AssertScanner_ReadFrom_StructFieldC(s.c, s.input[0]); err != nil {
+	if err = AssertScannerReadFromStructFieldC(s.c, s.input[0]); err != nil {
 		t.Error(err.Error())
 	}
 }
